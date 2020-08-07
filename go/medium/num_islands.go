0e7fbf2644e8d3db571a2084d6dfecc158709898
Sreejith Kesavan <sreejithemk@gmail.com>
2020-08-07 11:19:41 +0530

    * Update islands solution

--- go/medium/num_islands.go
+++ go/medium/num_islands.go
@@ -2,6 +2,30 @@
 // difficulty: Medium
 // leetcode_num: 200
 // leetcode_url: https://leetcode.com/problems/number-of-islands/
+//
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+// An island is surrounded by water and is formed by connecting adjacent lands
+// horizontally or vertically. You may assume all four edges of the grid are all
+// surrounded by water.
+//
+// Example 1:
+//
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+// Example 2:
+//
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
 
 package main
 
@@ -29,9 +53,9 @@ func expandIsland(grid [][]byte, i, j int) {
 		return
 	}
 	grid[i][j] = '0'
-	expandIsland(grid, i-1, j) // top expansion
+	// expandIsland(grid, i-1, j) // top expansion
 	expandIsland(grid, i+1, j) // bottom expansion
-	expandIsland(grid, i, j-1) // left expansion
+	// expandIsland(grid, i, j-1) // left expansion
 	expandIsland(grid, i, j+1) // right expansion
 }
 
@@ -41,7 +65,7 @@ func main() {
 		{'1', '1', '0', '1', '0'},
 		{'1', '1', '0', '0', '0'},
 		{'1', '1', '0', '0', '0'},
-		{'0', '0', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
 	}
 
 	fmt.Println(FindNumberOfIslands(grid))
