e6399df1c8e0ddf5c16bed303332643c41fa1d47
Sreejith Kesavan <sreejithemk@gmail.com>
2020-08-22 18:48:15 +0530

    * Update comments

--- python/easy/path_sum.py
+++ python/easy/path_sum.py
@@ -7,13 +7,13 @@
 #
 # Given a binary tree and a sum, determine if the tree has a root-to-leaf path
 # such that adding up all the values along the path equals the given sum.
-
+#
 # Note: A leaf is a node with no children.
-
+#
 # Example:
-
+#
 # Given the below binary tree and sum = 22,
-
+#
 #       5
 #      / \
 #     4   8
